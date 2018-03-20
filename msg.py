a1c0a25f2e485fc76b43b2b9c001296339587a71
Amit Singh Sansoya <tusharamit@yahoo.com>
2018-03-21 03:58:58 +0530

    initial commit

---
+++ msg.py
@@ -0,0 +1,9 @@
+import os
+from twilio.rest import Client
+msg="QR code was tried to be generated at Toll Tax Jaipur of Your Car"
+account_sid = "AC5adda6db593a0f369a427283c8436959"
+auth_token = "fc647306d1cd37646ac0f1f23c55e612"
+client = Client(account_sid, auth_token)
+client.api.account.messages.create(to="+13153538299",from_="+13153229683",body=msg)
+print("Message Sent")
+                
